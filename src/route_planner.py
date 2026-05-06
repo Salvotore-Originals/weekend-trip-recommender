7652ee65d3d9d7c45db46195ac446167fa8f5900
Salvotore-Originals <nitinnair89@gmail.com>
2026-05-06 22:33:33 +0530

    Update route_planner.py

--- src/route_planner.py
+++ src/route_planner.py
@@ -5,8 +5,10 @@ from src.ml_model import MLRecommender
 class RoutePlanner:
 
     def __init__(self, data):
+        self.data = data
         self.ml_model = MLRecommender(data)
 
+    # ✅ THIS MUST EXIST
     def get_nearby_places(self, destination, k):
         return self.ml_model.get_nearby_places_clean(destination, k)
 
